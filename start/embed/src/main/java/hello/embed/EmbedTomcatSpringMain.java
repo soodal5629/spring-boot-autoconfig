22338af8d0150cddbb1cb7f6c9e60b2c4c869245
chaelynjang <chaelynjang@lotte.net>
2023-07-11 17:38:18 +0900

    내장톰캣에 스프링 컨테이너 생성 및 디스패처 서블릿 연결해서 실행

---
+++ start/embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -0,0 +1,37 @@
+package hello.embed;
+
+import hello.spring.HelloConfig;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class EmbedTomcatSpringMain {
+
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatSpringMain.main");
+        // 톰캣 설정
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        // 스프링 컨테이너 생성
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        // HelloController가 스프링 컨테이너에 들어감
+        appContext.register(HelloConfig.class);
+
+        // 스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+        DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+
+        // 디스패처 서블릿 등록
+        // Context 설정은 톰캣에서 구동되는 하나의 웹 어플리케이션 자체의 설정을 의미
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "dispatcher", dispatcher);
+        context.addServletMappingDecoded("/", "dispatcher");
+
+        tomcat.start();
+    }
+}
