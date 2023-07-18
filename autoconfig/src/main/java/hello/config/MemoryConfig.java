9f0cdcdc790faa52fb4845a4ee36403ae88f0cc0
chaelynjang <chaelynjang@lotte.net>
2023-07-18 17:23:50 +0900

    스프링 부트가 제공하는 @ConditionalOnXxx 적용

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -3,12 +3,14 @@ package hello.config;
 import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class) // 특정 상황에서만 빈 등록
+//@Conditional(MemoryCondition.class) // 특정 상황에서만 빈 등록
+@ConditionalOnProperty(name = "memory", havingValue = "on") // 위에 주석 부분이랑 동일하게 동작
 public class MemoryConfig {
     @Bean
     public MemoryController memoryController() {
