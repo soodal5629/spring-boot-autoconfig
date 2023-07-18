e9ab6fcd753d8aaa5748b5d014eee768d0410bd6
chaelynjang <chaelynjang@lotte.net>
2023-07-18 13:34:05 +0900

    스프링부트 핵심원리 autoconfig 환경세팅

---
+++ autoconfig/src/main/java/hello/member/Member.java
@@ -0,0 +1,16 @@
+package hello.member;
+
+import lombok.Data;
+
+@Data
+public class Member {
+    private String memberId;
+    private String name;
+
+    public Member() {};
+
+    public Member(String memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+}
