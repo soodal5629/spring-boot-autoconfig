c1514b4774c5e32a0e12d9d7ccf5f35dec75db62
chaelynjang <chaelynjang@lotte.net>
2023-07-18 16:58:09 +0900

    특정 조건/상황일때만 외부 라이브러리(Memory)빈 등록되도록 설정

--- autoconfig/src/main/java/hello/config/MemoryConfig.java
+++ autoconfig/src/main/java/hello/config/MemoryConfig.java
@@ -1,11 +1,14 @@
 package hello.config;
 
+import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Conditional(MemoryCondition.class) // 특정 상황에서만 빈 등록
 public class MemoryConfig {
     @Bean
     public MemoryController memoryController() {
